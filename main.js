ed24a51f9e49ce6af6485a268c1590bd2fedcfa1
Jacob Dean <JacobDean117@gmail.com>
2019-02-08 16:07:01 -0600

    canvas test code

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
-    const screen = document.getElementById('screen');
-    const sxt = screen.getContext('2d');
+    const canvas = document.getElementById('canvas');
+    const cxt = canvas.getContext('2d');
 
-    sxt.fillstyle = 'blue';
-    sxt.fillRect(10, 10, 500, 500);
+
+    document.addEventListener('click', ()=>{
+        cxt.fillRect(200, 200, 5, 5,);
+    });
 
 });
