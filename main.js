07a87a086ae3acca23610441fecc452183aa7389
JakenPrime <JacobDean117@gmail.com>
2019-02-12 23:30:36 -0600

    added image folder and finalized background colar and basic game menu functionality

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', ()=>{
-
-    const canvas = document.getElementById('canvas');
-    const cxt = canvas.getContext('2d');
-
-
-    document.addEventListener('click', ()=>{
-        cxt.fillRect(200, 200, 5, 5,);
-    });
-
-});
